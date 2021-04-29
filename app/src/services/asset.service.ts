bee231f43bb3312623fe84111a095a1108ace357
Dennis Decoene <dennis.decoene@gmail.com>
2021-04-29 17:58:38 +0200

    use locaStorage to provice assets

--- app/src/services/asset.service.ts
+++ app/src/services/asset.service.ts
@@ -4,15 +4,48 @@ import { Asset } from 'src/interfaces/asset';
 import { portfolio } from 'src/data/portfolio';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AssetService {
+  constructor() {
+    this.init();
+  }
 
-  constructor() { }
+  private init() {
+    //save all items in portfolio to localstorage
+    portfolio.forEach((asset: Asset) => {
+      const tmpAsset = localStorage.getItem(asset.id.toString());
+      if (!tmpAsset) {
+        //not in storage, save
+        this.saveAsset(asset);
+      }
+    });
+  }
 
-  getAll(): Asset[] {
+  public getAll(): Asset[] {
+    //get everything out of localStorage
+    let myPortfolio: Asset[] = [];
+    const items = { ...localStorage };
 
-    return portfolio;
+    for (const key in items)
+    {
+       myPortfolio.push(JSON.parse(items[key]));
+    }
 
+    return myPortfolio;
+  }
+
+  public getAsset(id: number): Asset | null {
+    const tmpAsset = localStorage.getItem(id.toString());
+    if (tmpAsset) {
+      return JSON.parse(tmpAsset);
+    } else {
+      return null;
+    }
+  }
+
+  public saveAsset(asset: Asset): boolean {
+    localStorage.setItem(asset.id.toString(), JSON.stringify(asset));
+    return true;
   }
 }
